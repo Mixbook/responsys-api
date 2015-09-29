d6a3e973d0dfd93628a4652eeba36b548ffee54d
Pawel Kondzior <pawel@kondzior.com>
2015-09-29 23:59:07 +0200

    Disable SSL verification for Responsys [Mixbook/product#539]

--- lib/responsys/api/session.rb
+++ lib/responsys/api/session.rb
@@ -12,11 +12,12 @@ module Responsys
         }
 
         ssl_version = settings[:ssl_version] || :TLSv1
+        ssl_verify_mode = settings[:ssl_verify_mode] || :peer
 
         if settings[:debug]
-          @savon_client = Savon.client(wsdl: settings[:wsdl], element_form_default: :qualified, ssl_version: ssl_version, log_level: :debug, log: true, pretty_print_xml: true)
+          @savon_client = Savon.client(wsdl: settings[:wsdl], element_form_default: :qualified, ssl_version: ssl_version, log_level: :debug, log: true, pretty_print_xml: true, ssl_verify_mode: ssl_verify_mode)
         else
-          @savon_client = Savon.client(wsdl: settings[:wsdl], element_form_default: :qualified, ssl_version: ssl_version)
+          @savon_client = Savon.client(wsdl: settings[:wsdl], element_form_default: :qualified, ssl_version: ssl_version, ssl_verify_mode: ssl_verify_mode)
         end
       end
 
